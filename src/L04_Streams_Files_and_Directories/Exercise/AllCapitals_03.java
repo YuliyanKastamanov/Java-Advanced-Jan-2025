529a8f35f3b7f6bee079d29e7a3607fd74736c96
YuliyanKastamanov <92514049+YuliyanKastamanov@users.noreply.github.com>
2025-01-20 21:08:50 +0200

    Streams, Files and Directories - Exercise

---
+++ src/L04_Streams_Files_and_Directories/Exercise/AllCapitals_03.java
@@ -0,0 +1,30 @@
+package L04_Streams_Files_and_Directories.Exercise;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class AllCapitals_03 {
+    public static void main(String[] args) throws IOException {
+
+        String path = "L04_Streams_Files_and_Directories/Exercise/resources/input.txt";
+
+        String contentWithUpperCase = Files.readString(Path.of(path));
+
+        contentWithUpperCase = contentWithUpperCase.toUpperCase();
+
+        BufferedWriter writer = new BufferedWriter(new FileWriter("L04_Streams_Files_and_Directories/Exercise/resources/output.txt"));
+        writer.write(contentWithUpperCase);
+        writer.close();
+
+
+
+
+
+
+
+
+    }
+}
---
+++ src/L04_Streams_Files_and_Directories/Exercise/WordCount_06.java
@@ -0,0 +1,65 @@
+package L04_Streams_Files_and_Directories.Exercise;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class WordCount_06 {
+    public static void main(String[] args) throws IOException {
+
+        String pathWords = "L04_Streams_Files_and_Directories/Exercise/resources/words.txt";
+        String pathTwo = "L04_Streams_Files_and_Directories/Exercise/resources/text.txt";
+
+        Map<String, Integer> countWords = new HashMap<>();
+
+        List<String> allLinesWithWords = Files.readAllLines(Path.of(pathWords));
+
+        for (String line : allLinesWithWords){
+            Arrays.stream(line.split("\\s+"))
+                    .forEach(word ->
+                            countWords.put(word, 0));
+        }
+
+
+        List<String> allLinesFromText = Files.readAllLines(Path.of(pathTwo));
+
+        for (String line : allLinesFromText){
+            Arrays.stream(line.split("\\s+"))
+                    .forEach(word -> {
+                        if(countWords.containsKey(word)){
+                            int currentCount = countWords.get(word);
+                            countWords.put(word, currentCount + 1);
+                        }
+
+                    });
+        }
+
+        PrintWriter writer = new PrintWriter("L04_Streams_Files_and_Directories/Exercise/resources/result.txt");
+
+        countWords.entrySet().stream()
+                .sorted((e1, e2) -> e2.getValue().compareTo(e1.getValue()))
+                .forEach(entry -> writer.println(entry.getKey() + " - " + entry.getValue()));
+
+        writer.close();
+
+        //compareTo -> 0 (е1 == е2), 1 (е1 > е2), -1 (е2 > е1)
+        //sorted -> 0 (не разменя местата на елементите), 1 (разменя местата на е1 и е2), -1 (не разменя местата на елементите)
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
