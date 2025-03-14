64481878739a4a002f20c9ef3776df1be000f89a
Kaloyan Kostadinov <kaloqn_kostadinow@abv.bg>
2025-03-14 22:05:20 +0200

    added implementation of conqueringPeaks method in ClimbingImpl

--- src/OOP/L06_SOLID/lab/climbers/models/climbing/ClimbingImpl.java
+++ src/OOP/L06_SOLID/lab/climbers/models/climbing/ClimbingImpl.java
@@ -11,21 +11,21 @@ public class ClimbingImpl implements Climbing {
     public void conqueringPeaks(Mountain mountain, Collection<Climber> climbers) {
         Collection<String> peaksList = mountain.getPeaksList();
 
+        //•	Climbers cannot climb peaks if their strength is equal to or below 0.
+        //•	They start climbing and conquering peaks, one by one.
+        //•	If they conquer a peak, they add it to their roster of conquered peaks and their strength is decreased.
+        //•	The conquered peak should then be removed from the peaks list of the current mountain.
+        //•	Climbers cannot continue climbing if their strength drops to 0.
+        //o	If their strength drops to 0, the next climber starts climbing.
 
         for (Climber climber : climbers) {
 
-            // TODO remove the peak when is conquered
-            // TODO use iterator
-                for (String peak : mountain.getPeaksList()) {
-                    if (climber.canClimb()){
-                        climber.climb();
-                        climber.getRoster().getPeaks().add(peak);
-                    }else {
-                        break;
-                    }
-                }
-
+            while (climber.canClimb() && peaksList.iterator().hasNext()) {
+                climber.climb();
+                String peak = peaksList.iterator().next();
+                climber.getRoster().getPeaks().add(peak);
+                peaksList.remove(peak);
+            }
         }
     }
-
 }
